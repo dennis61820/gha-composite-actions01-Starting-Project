170241c55226340fd20c2105576a4110ebea0a96
dennis61820 <danngit@hotmail.com>
2024-04-23 15:05:14 -0500

    refactor: to js6

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,9 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+import { notice } from '@actions/core'
+import github from '@actions/github'
+import exec from '@actions/exec'
 
 function run() {
-  core.notice('Hello from my custom javascript action.')
+  notice('Hello from my custom javascript action.')
 }
 
 run()
