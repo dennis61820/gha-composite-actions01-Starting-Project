af06b6ad09a9046c58bb2b902ba6e86ba148fb7c
dennis61820 <danngit@hotmail.com>
2024-04-25 14:21:05 -0500

    added aws functionality

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,6 +6,15 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+  // 1) input values
+  const bucket = core.getInput('bucket', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+
+  // 2) upload files
+  const s3URI = `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
+
   core.notice('Hello from my custom javascript action.')
 }
 
